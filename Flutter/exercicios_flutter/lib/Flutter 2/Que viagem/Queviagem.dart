150c2528803e3ed1c24877ce9277655bec8b8501
EduduBR <dudubr0803@Gmail.com>
2022-11-01 16:19:50 -0300

    update

--- Flutter/exercicios_flutter/lib/Flutter 2/Que viagem/Queviagem.dart
+++ Flutter/exercicios_flutter/lib/Flutter 2/Que viagem/Queviagem.dart
@@ -20,12 +20,8 @@ class _MyTravolState extends State<MyTravol> {
             length: 3,
             child: Scaffold(
                 appBar: AppBar(
-                    title: Text(
-                      ' Que Viagem! Turismo ',
-                      style: GoogleFonts.notoSerif(
-                        fontSize: 25,
-                      ),
-                    ),
+                    title: Text(' Que Viagem! Turismo ',
+                        style: GoogleFonts.notoSerif(fontSize: 25)),
                     centerTitle: true,
                     titleSpacing: 00.0,
                     toolbarHeight: 60.2,
@@ -55,49 +51,46 @@ class _MyTravolState extends State<MyTravol> {
                   Center(
                       child: Column(children: [
                     Container(
-                        margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                        margin: const EdgeInsets.only(top: 50),
                         child: Column(children: [
                           Text('Partida',
-                              style: GoogleFonts.notoSerif(
-                                fontSize: 30,
-                              )),
+                              style: GoogleFonts.notoSerif(fontSize: 30)),
                           Text('Outubro 31,2022',
                               style: GoogleFonts.notoSerif(
                                 fontSize: 20,
                               ))
                         ])),
-                    Row(
-                      children: [
-                        Container(
-                            margin: const EdgeInsets.fromLTRB(55, 50, 0, 0),
-                            child: Column(children: [
-                              Text('GRU',
-                                  style: GoogleFonts.notoSerif(
-                                    fontSize: 50,
-                                  )),
-                              Text('Guarulhos',
-                                  style: GoogleFonts.notoSerif(
-                                    fontSize: 20,
-                                  ))
-                            ])),
-                        const Icon(
-                          Icons.connecting_airports_outlined,
-                          size: 80,
-                        ),
-                        Container(
-                            margin: const EdgeInsets.only(top: 50),
-                            child: Column(children: [
-                              Text('OPO',
-                                  style: GoogleFonts.notoSerif(
-                                    fontSize: 50,
-                                  )),
-                              Text('Porto, Portugal',
-                                  style: GoogleFonts.notoSerif(
-                                    fontSize: 20,
-                                  ))
-                            ]))
-                      ],
-                    )
+                    Row(children: [
+                      Container(
+                          margin: const EdgeInsets.fromLTRB(25, 100, 0, 0),
+                          child: Column(children: [
+                            Text('GRU',
+                                style: GoogleFonts.notoSerif(fontSize: 50)),
+                            Text('Guarulhos',
+                                style: GoogleFonts.notoSerif(fontSize: 20))
+                          ])),
+                      Container(
+                          margin: const EdgeInsets.fromLTRB(10, 100, 10, 0),
+                          child: Image.network(
+                            'https://cdn-icons-png.flaticon.com/512/2685/2685659.png',
+                            scale: 5,
+                          )),
+                      Container(
+                          margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+                          child: Column(children: [
+                            Text('OPO',
+                                style: GoogleFonts.notoSerif(fontSize: 50)),
+                            Text('Porto, Portugal',
+                                style: GoogleFonts.notoSerif(fontSize: 20)),
+                          ])),
+                    ]),
+                    Card(
+                        margin: const EdgeInsets.only(top: 60),
+                        color: const Color.fromARGB(255, 170, 141, 45),
+                        child: Image.network(
+                          'https://cdn-icons-png.flaticon.com/512/202/202928.png',
+                          scale: 3,
+                        )),
                   ])),
                   ListView(
                     padding: const EdgeInsets.all(8),
@@ -105,51 +98,64 @@ class _MyTravolState extends State<MyTravol> {
                       Column(
                         children: [
                           Text('Pontos turísticos do Porto',
-                              style: GoogleFonts.breeSerif(
-                                fontSize: 28,
-                              )),
+                              style: GoogleFonts.breeSerif(fontSize: 28)),
                           const Divider(
                             color: Color.fromARGB(255, 87, 19, 19),
                             indent: 10,
-                            endIndent: 10,thickness: 4,
+                            endIndent: 10,
+                            thickness: 4,
                           ),
                           Text('Ponte Dom Luiz I',
                               style: GoogleFonts.lobster(
-                                fontSize: 35,color:  const Color.fromARGB(255, 170, 141, 45)
-                              )),
-                          Image.network(
-                              'https://viagemeturismo.abril.com.br/wp-content/uploads/2017/05/istock-533960357-1.jpg?quality=70&strip=info&w=1024&resize=1200,800'),
+                                  fontSize: 35,
+                                  color:
+                                      const Color.fromARGB(255, 170, 141, 45))),
+                          Image.network(Viagem.imagem1),
                           Text(Viagem.texto1,
                               style: GoogleFonts.breeSerif(
                                 fontSize: 25,
                               )),
-                          const Divider(thickness: 4,
+                          const Divider(
+                            thickness: 4,
                             color: Color.fromARGB(255, 87, 19, 19),
                             indent: 10,
                             endIndent: 10,
-                          ),Text('Igreja e Torre dos Clérigos', style: GoogleFonts.lobster(
-                                fontSize: 35,color:  const Color.fromARGB(255, 170, 141, 45)
-                              )),
-                          Image.network(
-                              'https://s7a5n8m2.stackpathcdn.com/wp-content/uploads/2015/09/1-5.jpg'),
-                              Text(Viagem.texto2,style: GoogleFonts.breeSerif(
+                          ),
+                          Text('Igreja e Torre dos Clérigos',
+                              style: GoogleFonts.lobster(
+                                  fontSize: 35,
+                                  color:
+                                      const Color.fromARGB(255, 170, 141, 45))),
+                          Image.network(Viagem.imagem2),
+                          Text(Viagem.texto2,
+                              style: GoogleFonts.breeSerif(
                                 fontSize: 25,
-                              )), const Divider(thickness: 4,
+                              )),
+                          const Divider(
+                            thickness: 4,
                             color: Color.fromARGB(255, 87, 19, 19),
                             indent: 10,
                             endIndent: 10,
-                          ),Text('Livraria Lello', style: GoogleFonts.lobster(
-                                fontSize: 35,color:  const Color.fromARGB(255, 170, 141, 45)
-                              )),
-                          Image.network(
-                              'https://mapadomundo.org/wp-content/uploads/2017/01/Lello-capa.jpg'),
-                              Text(Viagem.texto3,style: GoogleFonts.breeSerif(
+                          ),
+                          Text('Livraria Lello',
+                              style: GoogleFonts.lobster(
+                                  fontSize: 35,
+                                  color:
+                                      const Color.fromARGB(255, 170, 141, 45))),
+                          Image.network(Viagem.imagem3),
+                          Text(Viagem.texto3,
+                              style: GoogleFonts.breeSerif(
                                 fontSize: 25,
                               ))
                         ],
                       )
                     ],
-                  ),Image.network('https://i.imgur.com/qVbQ7ho.png'),
-                ]))));
+                  ),
+                  Image.network('https://i.imgur.com/qVbQ7ho.png'),
+                ]
+              )
+            )
+          )
+        );
   }
 }
--- Flutter/exercicios_flutter/lib/Flutter 2/Que viagem/textos.dart
+++ Flutter/exercicios_flutter/lib/Flutter 2/Que viagem/textos.dart
@@ -3,5 +3,12 @@ class Viagem {
       'A Ponte D. Luís I, é uma ponte em estrutura metálica com dois tabuleiros, construída entre os anos 1881 e 1886, ligando as cidades do Porto e Vila Nova de Gaia separadas pelo rio Douro, em Portugal.';
   static const String texto2 =
       'A Igreja e Torre dos Clérigos é um notável conjunto arquitetónico situado na cidade do Porto, Portugal, sendo considerado o cartão-postal dessa cidade. O conjunto localiza-se no topo da Rua dos Clérigos, entre as ruas de São Filipe Néri e da Assunção.';
-  static const String texto3 = 'A Livraria Lello situa-se no número 144 da Rua das Carmelitas, no Centro Histórico da cidade do Porto, em Portugal. Classificada como Monumento de Interesse Público, e em vias de se tornar Monumento Nacional, a Livraria Lello preserva a beleza original do seu edifício.';
+  static const String texto3 =
+      'A Livraria Lello situa-se no número 144 da Rua das Carmelitas, no Centro Histórico da cidade do Porto, em Portugal. Classificada como Monumento de Interesse Público, e em vias de se tornar Monumento Nacional, a Livraria Lello preserva a beleza original do seu edifício.';
+  static const String imagem3 =
+      'https://mapadomundo.org/wp-content/uploads/2017/01/Lello-capa.jpg';
+  static const String imagem2 =
+'https://s7a5n8m2.stackpathcdn.com/wp-content/uploads/2015/09/1-5.jpg';
+  static const String imagem1 =
+       'https://viagemeturismo.abril.com.br/wp-content/uploads/2017/05/istock-533960357-1.jpg?quality=70&strip=info&w=1024&resize=1200,800';
 }
